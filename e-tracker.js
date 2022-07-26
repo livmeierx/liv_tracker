26e0bea51006ec40dabba95f04f07c45df8ead9b
livmeierx <liv.meier13@gmail.com>
2022-07-26 17:37:14 -0400

    added layout of functions

--- e-tracker.js
+++ e-tracker.js
@@ -101,6 +101,111 @@ function mainMenu() {
             deleteDept();
             break;
         }
-    })
+    });
 }
+
+
+// function to view all employees
+function viewAllEmp() {
+    let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+
+    connection.query(query, function(err, res) {
+        if(err) return err;
+        console.log("\n");
+
+        console.table(res);
+
+        mainMenu();
+    });
+}
+
+// function to view employees by dept
+function viewAllEmp() {
+    let deptArr = [];
+
+    promisemysql.createConnection(connectionProp)
+    .then((conn) => {
+        return conn.query('SELECT name FROM department');
+    }).then(function(value){
+        deptQuery = value;
+        for (i=0; i <value.length; i++){
+            deptArr.push(value[i].name);
+        }
+    }).then(() => {
+        inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "Which department would you like to search?",
+            choices: deptArr
+        })    
+        .then((answer) => {
+            const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${answer.department}' ORDER BY ID ASC`;
+            connection.query(query, (err, res) => {
+                if(err) return err;
+                
+                console.log("\n");
+                console.table(res);
+
+                mainMenu();
+            });
+        });
+    });   
+}
+
+// function to view employees by role
+function viewAllEmpRole() {
+
+}
+
+// function to add an employee
+function addEmp() {
+
+}
+
+// function to add a role
+function addRole() {
+
+}
+
+// function to add a department
+function addDept() {
+
+}
+
+// function to update role
+function updateRole() {
+
+}
+
+// function to update manager
+function updateEmpManager() {
+
+}
+
+// function to view all employees by manager
+function viewAllEmpManager() {
+
+}
+
+// function to delete an employee
+function deleteEmp() {
+
+}
+
+// function to delete a role
+function deleteRole() {
+
+}
+
+// function to delete a department
+function deleteDept() {
+
+}
+
+// function to view department budget
+function viewDeptBudget() {
+
+}
+
+
 
